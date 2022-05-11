715852baecdc104a084766e0c52c388504a0a9df
doreenkam <dkamushinda@gmail.com>
2022-05-11 16:39:47 +0100

    Updated

---
+++ src/components/RepoDetails/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function RepoDetails({ details, loading }) {
+  if (loading) {
+    return <h1 className="loader">Loading...</h1>;
+  } else {
+    return (
+      <div className="repo-details-container">
+        <div className="details-row">
+          <label className="label">Name: </label>
+          <span className="value">{details.name}</span>
+        </div>
+        <div className="details-row">
+          <label className="label">Fork Count: </label>
+          <span className="value">{details.forks}</span>
+        </div>
+        <div className="details-row">
+          <label className="label">Language: </label>
+          <span className="value">{details.language}</span>
+        </div>
+        <div className="details-row">
+          <label className="label">Stars: </label>
+          <span className="value">{details.stargazers_count}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RepoDetails;
