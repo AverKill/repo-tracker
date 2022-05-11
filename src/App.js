163ad6a8e6de06ca014eaa2f25e696066bd3bcc0
doreenkam <dkamushinda@gmail.com>
2022-05-11 16:43:53 +0100

    Added pages and installed babel presets

--- src/App.js
+++ src/App.js
@@ -1,77 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import axios from "axios";
+import { default as Layout } from "./layouts";
+import * as Pages from "./pages";
 
-import RepoDetails from "./RepoDetails";
 import "./App.css";
 
-function App() {
-  const [username, setUsername] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [repos, setRepos] = useState([]);
-  const [details, setDetails] = useState({});
-  const [detailsLoading, setDetailsLoading] = useState(false);
-
-  useEffect(() => {
-    setRepos([]);
-    setDetails({});
-  }, [username]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    searchRepos();
-  }
-
-  function searchRepos() {
-    setLoading(true);
-    axios({
-      method: "get",
-      url: `https://api.github.com/users/${username}/repos`,
-    }).then((res) => {
-      setLoading(false);
-      setRepos(res.data);
-    });
-  }
-
-  function renderRepo(repo) {
-    return (
-      <div className="row" onClick={() => getDetails(repo.name)} key={repo.id}>
-        <h2 className="repo-name">{repo.name}</h2>
-      </div>
-    );
-  }
-
-  function getDetails(repoName) {
-    setDetailsLoading(true);
-    axios({
-      method: "get",
-      url: `https://api.github.com/repos/${username}/${repoName}`,
-    }).then((res) => {
-      setDetailsLoading(false);
-      setDetails(res.data);
-    });
-  }
+const App = () => {
   return (
-    <div className="page">
-      <div className="landing-page-container">
-        <div className="left-siee">
-          <form className="form">
-            <input
-              className="input"
-              value={username}
-              placeholder="GitHub Username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <button className="button" onClick={handleSubmit}>
-              {loading ? "Searching..." : "Search"}
-            </button>
-          </form>
-          <div className="results-container">{repos.map(renderRepo)}</div>
-          <RepoDetails details={details} loading={detailsLoading} />
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Pages.IndexPage />} />
+        <Route path="*" element={<Pages.NotFoundPage />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/pages/IndexPage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { FormCard } from "../../components";
+
+const IndexPage = () => {
+  return <FormCard />;
+};
+
+export default IndexPage;
