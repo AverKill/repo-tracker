cc0d85fb3149dd74cf9b4015a2446bb2dc62ff51
doreenkam <dkamushinda@gmail.com>
2022-05-11 11:34:03 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+import RepoDetails from "./RepoDetails";
 import "./App.css";
 
 function App() {
   const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+  const [details, setDetails] = useState({});
+  const [detailsLoading, setDetailsLoading] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
     searchRepos();
   }
 
-  function seaarchRepos() {
+  function searchRepos() {
     setLoading(true);
     axios({
       method: "get",
@@ -21,6 +27,24 @@ function App() {
     });
   }
 
+  function renderRepo(repo) {
+    return (
+      <div className="row" onClick={() => getDetails(repo.name)} key={repo.id}>
+        <h2 className="repo-name">{repo.name}</h2>
+      </div>
+    );
+  }
+
+  function getDetails(repoName) {
+    setDetailsLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/repos${username}/${repoName}`,
+    }).then((res) => {
+      setDetailsLoading(false);
+      setDetails(res.data);
+    });
+  }
   return (
     <div className="page">
       <div className="landing-page-container">
@@ -36,6 +60,8 @@ function App() {
               {loading ? "Searching..." : "Search"}
             </button>
           </form>
+          <div className="results-container">{repos.map(renderRepo)}</div>
+          <RepoDetails details={details} loading={detailsLoading} />
         </div>
       </div>
     </div>
